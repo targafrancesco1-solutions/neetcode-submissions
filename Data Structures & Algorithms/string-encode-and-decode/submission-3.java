6ff6e948113942e2606cda6814dc50034aca53fc
targafrancesco1-solutions <targafrancesco1@gmail.com>
2026-09-17 11:20:07 +0200

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    public String encode(List<String> strs){
+
+    StringBuilder sb = new StringBuilder();
+    
+        for(String s : strs){
+            sb.append(s.length() + "#");
+            sb.append(s);
+    }
+
+    return sb.toString();
+   }
+
+    public List<String> decode(String str){
+        
+        List<String> result = new ArrayList<String>();
+        char c = '#';
+        int i = 0;
+        while (i < str.length()) {
+            int j = i;
+            while (str.charAt(j) != '#') {
+                j++;
+            }
+            
+               int l = Integer.parseInt(str.substring(i,j));
+                i = j +1;
+                j = i+l;
+               result.add(str.substring(i,j));
+                i = j;
+            
+        }
+
+        return result;
+    }
+}
